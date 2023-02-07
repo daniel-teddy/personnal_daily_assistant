641e33eabf31fc9b21e1273b4aa39e7bae559d62
teddy-webdev <89258830+teddy-webdev@users.noreply.github.com>
2023-02-07 18:36:52 +0200

    update

---
+++ calendar.js
@@ -0,0 +1,127 @@
+const isLeapYear = (year) => {
+    return (
+      (year % 4 === 0 && year % 100 !== 0 && year % 400 !== 0) ||
+      (year % 100 === 0 && year % 400 === 0)
+    );
+};
+
+  const getFebDays = (year) => {
+    return isLeapYear(year) ? 29 : 28;
+  };
+  let calendar = document.querySelector('.calendar');
+  const month_names = [
+    'January',
+    'February',
+    'March',
+    'April',
+    'May',
+    'June',
+    'July',
+    'August',
+    'September',
+    'October',
+    'November',
+    'December',
+  ];
+  let month_picker = document.querySelector('#month-picker');
+  const dayTextFormate = document.querySelector('.day-text-formate');
+  const timeFormate = document.querySelector('.time-formate');
+  const dateFormate = document.querySelector('.date-formate');
+  const localFormate = document.querySelector('.date-formate-local');
+  
+ 
+  
+  const generateCalendar = (month, year) => {
+    let calendar_days = document.querySelector('.calendar-days');
+    calendar_days.innerHTML = '';
+    let calendar_header_year = document.querySelector('#year');
+    let days_of_month = [
+      31,
+      getFebDays(year),
+      31,
+      30,
+      31,
+      30,
+      31,
+      31,
+      30,
+      31,
+      30,
+      31,
+    ];
+    
+    let currentDate = new Date();
+    
+    month_picker.innerHTML = month_names[month];
+    
+    calendar_header_year.innerHTML = year;
+    
+    let first_day = new Date(year, month);
+  
+  
+  for (let i = 0; i <= days_of_month[month] + first_day.getDay() - 1; i++) {
+  
+      let day = document.createElement('div');
+  
+      if (i >= first_day.getDay()) {
+        day.innerHTML = i - first_day.getDay() + 1;
+
+        if (i - first_day.getDay() + 1 === currentDate.getDate() &&
+          year === currentDate.getFullYear() &&
+          month === currentDate.getMonth()
+        ) {
+          day.classList.add('current-date');
+        }
+      }
+      calendar_days.appendChild(day);
+    }
+  };
+  
+  document.querySelector('#pre-month').onclick = () => {
+    --thiscurrentMonth.value;
+    generateCalendar(thiscurrentMonth.value, thiscurrentYear.value);
+  };
+  document.querySelector('#next-month').onclick = () => {
+    ++thiscurrentMonth.value;
+    generateCalendar(thiscurrentMonth.value, thiscurrentYear.value);
+  };
+  
+  let currentDate = new Date();
+  let thiscurrentMonth = { value: currentDate.getMonth() };
+  let thiscurrentYear = { value: currentDate.getFullYear() };
+  generateCalendar(thiscurrentMonth.value, thiscurrentYear.value);
+
+  const thistodayShowTime = document.querySelector('.time-formate');
+  const thistodayShowDate = document.querySelector('.date-formate');
+  const thisNowShowDate = document.querySelector('.date-formate-local');
+  
+  const currshowDate = new Date();
+  const showCurrentDateOption = {
+    year: 'numeric',
+    month: 'long',
+    day: 'numeric',
+    weekday: 'long',
+  };
+  const currentDateFormate = new Intl.DateTimeFormat(
+    'en-US',
+    showCurrentDateOption
+  ).format(currshowDate);
+  thistodayShowDate.textContent = currentDateFormate;
+  
+  setInterval(() => {
+    const timer = new Date();
+    const option = {
+      hour: 'numeric',
+      minute: 'numeric',
+      second: 'numeric',
+    };
+    const formateTimer = new Intl.DateTimeFormat('en-us', option).format(timer);
+   /*  let time = `${`${timer.getHours()}`.padStart(
+      2,
+      '0'
+    )}:${`${timer.getMinutes()}`.padStart(
+      2,
+      '0'
+    )}: ${`${timer.getSeconds()}`.padStart(2, '0')}`; */
+    thistodayShowTime.textContent = formateTimer;
+  }, 1000);
